9e29201dde1f35bfd37f2b29d7bca6061dbeab7f
Shantanu Jadhav <shantanujadhav283@gmail.com>
2026-08-10 09:53:06 +0530

    Add MongoDB session with connect-mongo

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const path = require("path");
 const session = require("express-session");
-const MongoStore = require("connect-mongo");
+const MongoStore = require("connect-mongo").default;
 const flash = require("connect-flash");
 const ExpressError = require("./utils/ExpressError");
 
